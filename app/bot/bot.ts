02d97c63c097ed1d13dabd457ddf102514fa7bf9
Jean-Simon Marrocco <js_marrocco@hotmail.com>
2018-10-06 17:45:08 -0400

    thief mode

--- app/bot/bot.ts
+++ app/bot/bot.ts
@@ -11,6 +11,12 @@ enum botStates {
     goHomeFromSteal
 }
 
+const down: Point = new Point (0, 1);
+const up: Point = new Point (0, -1);
+const left: Point = new Point (-1, 0);
+const right: Point = new Point (1, 0);
+
+
 export class Bot {
     protected playerInfo: Player;
 
@@ -19,6 +25,7 @@ export class Bot {
     private state: botStates = botStates.goSteal;
     private moving: boolean = false;
     private moved: boolean = false;
+    private oldCarriedResources = 0;
 
     /**
      * Gets called before ExecuteTurn. This is where you get your bot's state.
@@ -35,87 +42,7 @@ export class Bot {
      * @returns string The action to take(instanciate them with AIHelper)
      */
     public executeTurn(map: GameMap, visiblePlayers: Player[]): string {
-        /*
-        if (this.state === botStates.goSteal) {
-            const point: Point = new Point(this.playerInfo.Position.x + 1, this.playerInfo.Position.y);
-            const right: Point = new Point (1, 0);
-            switch (map.getTileAt(point)) {
-                case TileContent.Wall:
-                    return AIHelper.createAttackAction(right);
-                case TileContent.Resource:
-                    return AIHelper.createCollectAction(right);
-                case TileContent.Empty:
-                    return AIHelper.createMoveAction(right);
-                case TileContent.Player:
-                    return AIHelper.createAttackAction(right);
-                case TileContent.House:
-                    this.state = botStates.goHomeFromSteal;
-                    return AIHelper.createStealAction(right);
-                default:
-                    return AIHelper.createEmptyAction();
-            }
-        } else if (this.state === botStates.goHomeFromSteal) {
-            const point: Point = new Point(this.playerInfo.Position.x - 1, this.playerInfo.Position.y);
-            const left: Point = new Point (-1, 0);
-            switch (map.getTileAt(point)) {
-                case TileContent.Wall:
-                    return AIHelper.createAttackAction(left);
-                case TileContent.Empty:
-                    return AIHelper.createMoveAction(left);
-                case TileContent.Resource:
-                    return AIHelper.createCollectAction(left);
-                case TileContent.Player:
-                    return AIHelper.createAttackAction(left);
-                case TileContent.House:
-                    this.state = botStates.goSteal;
-                    return AIHelper.createMoveAction(left);
-                default:
-                    return AIHelper.createEmptyAction(); 
-            }
-        }*//* else if (this.currentPath.length === 0 && !this.moving) {
-            // find assets positionf
-            const maperonni: Map<TileContent, Point[]> = BotHelper.getAssets(map, this.playerInfo);
-
-            let content: TileContent;
-            if (this.state === botStates.goMining) {
-                content = TileContent.Resource;
-            } 
-            else {
-                content = TileContent.House;
-            }
-
-            maperonni.get(content).forEach(point => {
-                const path = [[1,1], [1,2], [2,2]];
-                if (path.length < this.currentPath.length) {
-                    this.currentPath = path;
-                }
-            });
-
-            if (this.state === botStates.goMining) {
-                this.currentResourcePoint = this.currentPath.pop();
-            } 
-            
-            this.moving = true;
-
-        }
-
-        if (this.currentPath.length !== 0) {
-            const currentPosition: number[] = this.currentPath.shift();
-            return AIHelper.createMoveAction( BotHelper.nextMove(currentPosition, this.currentPath[0]));
-        } else {
-            if (this.state === botStates.goMining) {
-                this.state = botStates.goHome;
-                return AIHelper.createCollectAction(BotHelper.nextMove([this.playerInfo.Position.x, this.playerInfo.Position.y], this.currentResourcePoint));
-            } else if (this.state === botStates.goHome) {
-                this.state = botStates.goMining;
-            } 
-            this.moving = false;
-        }*/
-        if(!this.moved) {
-            this.moved =true;
-            return AIHelper.createMoveAction(new Point(0,1));
-        }
-        return AIHelper.createEmptyAction();
+        return this.thiefMode(map, visiblePlayers);
     }
 
     /**
@@ -123,4 +50,46 @@ export class Bot {
      * @returns void
      */
     public afterTurn(): void { }
+
+    private thiefMode(map: GameMap, visiblePlayers: Player[]): string {
+        if (this.state === botStates.goSteal) {
+            const point: Point = new Point(this.playerInfo.Position.x + 1, this.playerInfo.Position.y);
+            switch (map.getTileAt(point)) {
+                case TileContent.Wall:
+                    return AIHelper.createAttackAction(down);
+                case TileContent.Resource:
+                    return AIHelper.createCollectAction(down);
+                case TileContent.Empty:
+                    return AIHelper.createMoveAction(down);
+                case TileContent.Player:
+                    return AIHelper.createAttackAction(down);
+                case TileContent.House:
+                    if (this.playerInfo.CarriedResources === this.oldCarriedResources) {
+                        this.state = botStates.goHomeFromSteal;
+                        return AIHelper.createMoveAction(up);
+                    }
+                    this.oldCarriedResources = this.playerInfo.CarriedResources;
+                    return AIHelper.createStealAction(down);
+                default:
+                    return AIHelper.createEmptyAction();
+            }
+        } else if (this.state === botStates.goHomeFromSteal) {
+            const point: Point = new Point(this.playerInfo.Position.x - 1, this.playerInfo.Position.y);
+            switch (map.getTileAt(point)) {
+                case TileContent.Wall:
+                    return AIHelper.createAttackAction(up);
+                case TileContent.Empty:
+                    return AIHelper.createMoveAction(up);
+                case TileContent.Resource:
+                    return AIHelper.createCollectAction(up);
+                case TileContent.Player:
+                    return AIHelper.createAttackAction(up);
+                case TileContent.House:
+                    this.state = botStates.goSteal;
+                    return AIHelper.createMoveAction(up);
+                default:
+                    return AIHelper.createEmptyAction(); 
+            }
+        }
+    }
 }
